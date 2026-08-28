6175bb84d48774fb5a0be0d16f363fdeb94a511b
Raikobit <35585188+samcris@users.noreply.github.com>
2026-08-28 16:40:00 -0500

    Add files via upload

---
+++ MochilaValorizada/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void cargabin(int num,int n,int *cromo) {
+    int res,i=0;
+    for (int j=0; j<n; j++) cromo[j]=0;
+    while (num>0) {
+        res=num%2;
+        num=num/2;
+        cromo[i]=res;
+        i++;
+    }
+}
+
+int main() {
+    int peso=15,mpeso=0,mcomb;
+    int paq[]={1,2,1,4,12};
+    double valor[]={10,5,5,10,2};
+    int n=sizeof(paq)/sizeof(paq[0]);
+    int comb=(int)pow(2,n);
+    int cromo[n];
+    double mvalor;
+    for (int i=0; i<comb; i++) {
+        int pparcial=0;
+        double pvalor=0;
+        cargabin(i,n,cromo);
+        for (int j=0; j<n; j++) {
+            pparcial+=cromo[j]*paq[j];
+            pvalor+=cromo[j]*valor[j];
+        }
+        if (pparcial<=peso and pvalor>mvalor) {
+            mvalor=pvalor;
+            mcomb=i;
+        }
+    }
+    cout<<"El mejor peso es:"<<mvalor<<endl;
+    cargabin(mcomb,n,cromo);
+    cout <<"Los paquetes seleccionados son: ";
+    for (int i=0; i<n; i++)
+        if (cromo[i]==1)
+            cout<<valor[i]<<" ";
+
+    return 0;
+}
+
